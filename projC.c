ceb9e7a4c474a244a794a6da7e22f517dd3298c0
pichard Antoine <antoinexvipichard@gmail.com>
2023-02-27 11:23:11 +0100

    fonctionnel, juste isbomb ps opti et manque de couleurs

--- projC.c
+++ projC.c
@@ -4,45 +4,89 @@
 #include <time.h>
 
 void printNumero();
+void tabActuel(char tab[], int bomb[], int idBomb);
 
 
 
 int main()
 {
     // On commence par initialiser le générateur de nombre pseudo-aléatoires.
-    //srand(time(NULL));
+    srand(time(NULL));
+    while (true)
+    {
+        while (true) {
+            int bomb[20];                           //initialise le tab des bombes
+            int idBomb = 0;
 
-    while (true) {
-        /*
-        // Tirer une valeur aléatoire entre 0 et 100.
-        int searchedValue = rand() % 101;
-        */
-        char* tab[100];
-        for (int i = 0; i < 100; i++)
-        {
-            tab[i] = "X";
-        }
+            // Tirer une valeur aléatoire entre 0 et 100.
+            int bombe = rand() % 101;
+            bomb[idBomb] = bombe;                //stocke les cases des bombes dans un tableau
+            idBomb += 1;
 
-        printNumero();
 
-        for (int i = 0; i < 100; i++)
-        {
-            printf("%s", tab[i]);
 
-            if (((i + 1) % 10) == 0)
+            char tab[100];
+            for (int i = 0; i < 100; i++)
             {
-                printf("\n");
-
+                tab[i] = "|X|";
             }
+
+            printNumero();
+
+            for (int i = 0; i < 100; i++)
+            {
+                printf("%c", tab[i]);
+
+                if (((i + 1) % 10) == 0)
+                {
+                    printf("\n");
+
+                }
+            }
+            printf("choisisez la case a detruire\n");
+            int choix;
+            if ((scanf_s("%d", &choix)) != 1)
+            {
+                printf("erreur de scan\n");
+                return EXIT_FAILURE;
+            }
+            while (choix >= 100)
+            {
+                printf("erreur, choissisez une case du tableau non utilisé\n");
+                if ((scanf_s("%d", &choix)) != 1)
+                {
+                    printf("erreur de scan\n");
+                    return EXIT_FAILURE;
+                }
+            }
+            for (int k = idBomb; k >= 0; k--)
+            {
+                if (bomb[k] == choix)
+                {
+                    printf("bombe touche. Game over. la valeur de la bombe est %d\n", bomb[k]);
+                    return 0;
+                }
+            }
+
+            printf("bombe non touche. Bravo,continue!\n");
+            tab[choix] = "| |";
+
+            tabActuel(tab, bomb, idBomb);
+
         }
-
-        int sortie = 0;
-        if (sortie == 0) break;
-
+        printf("Game Over. Belle partie.voulez vous recommencer ?\n");
+        int sortie;
+        if ((scanf_s("%d", &sortie)) != 1)
+        {
+            printf("erreur de scan\n");
+            return EXIT_FAILURE;
+        }
+        if (sortie == 0)
+        {
+            break;
+        }
     }
-
-    printf("GG");
-
+    printf("Fin de partie.\n");
     return EXIT_SUCCESS;
 }
 
@@ -50,7 +94,68 @@ void printNumero()
 {
     for (int i = 1; i <= 10; i++)
     {
-        printf("%d", i);
+        printf("|%d|", i);
     }
     printf("\n");
+}
+
+void tabActuel(char tab[], int bomb[], int idBomb)  //actualise le tab en calculant les numero indices des cases en fonction du tab ancien, de la liste de bomb et l'id de la derniere bombe du tab de bomb
+{
+    for (int i = 0; i <= 100; i++)
+    {
+        if (tab[i] == "| |")         //si la case est vide...
+        {
+            int countBomb = 0;
+            for(int k = idBomb; k >= 0; k--)                    //...claculer le nombre de bombes proches
+            {
+                if (tab[i - 11] == bomb[k])
+                {
+                    countBomb += 1;
+                }
+
+                if (tab[i - 10] == bomb[k])
+                {
+                    countBomb += 1;
+                }
+
+                if (tab[i - 9] == bomb[k])
+                {
+                    countBomb += 1;
+                }
+
+                if (tab[i - 1] == bomb[k])
+                {
+                    countBomb += 1;
+                }
+
+                if (tab[i + 1] == bomb[k])
+                {
+                    countBomb += 1;
+                }
+
+                if (tab[i + 9] == bomb[k])
+                {
+                    countBomb += 1;
+                }
+                
+                if (tab[i + 10] == bomb[k])
+                {
+                    countBomb += 1;
+                }
+                
+                if (tab[i + 11] == bomb[k])
+                {
+                    countBomb += 1;
+                }
+            }
+            tab[i] = "|(char)countBomb|";
+            printf("%c", tab[i]);
+        }
+        printf("%c", tab[i]);
+        if (((i + 1) % 10) == 0)
+        {
+            printf("\n");
+
+        }
+    }
 }
